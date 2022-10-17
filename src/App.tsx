2195639322964b72c06ee214e6a2ce5fa4278d5c
naughtymor <znaknikita.it@gmail.com>
2022-10-17 23:14:35 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {TodoList} from "./TodoList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const tasks1 = [
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "ReactJS", isDone: false}
+    ]
+
+    const tasks2 = [
+        {id: 1, title: "Happy Birthday!", isDone: false},
+        {id: 2, title: "Smells like teen spirit", isDone: true},
+        {id: 3, title: "u mad!", isDone: false}
+    ]
+
+    return (
+        <div className="App">
+            <TodoList title={'What to learn'} tasks={tasks1}/>
+            <TodoList title={'Songs'} tasks={tasks2}/>
+        </div>
+    );
 }
 
 export default App;
